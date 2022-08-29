388c01848ec62306a6b40201d782b069cc6441f3
colingithub <colinga7@yahoo.co.uk>
2022-08-29 15:38:50 +0100

    Update Program.cs
    CORS Policy  Fix   add  https://red-river-047703010.1.azurestaticapps.net

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -7,11 +7,11 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CORSPolicy", 
         builder =>
-        {
+        
             builder
             .AllowAnyMethod()
             .AllowAnyHeader()
-            .WithOrigins("http://localhost:3000", "https://calm-water-04859b403.azurestaticapps.net");
+            .WithOrigins("http://localhost:3000", "https://red-river-047703010.1.azurestaticapps.net");
         });
 });
 
